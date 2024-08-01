240627053e01626c96b45fea6c36d1d37defd7a9
X-GORB-studio <151430256+zls-wqx@users.noreply.github.com>
2024-08-01 19:30:43 +0800

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     // 导航功能
-    const links = document.querySelectorAll('nav ul li a');
+    const navContainer = document.getElementById('nav-container');
 
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            window.location.href = link.getAttribute('href');
-        });
-    });
+    if (navContainer) {
+        fetch('nav.html')
+            .then(response => response.text())
+            .then(data => {
+                navContainer.innerHTML = data;
+
+                // 为所有导航链接添加事件监听器
+                const links = navContainer.querySelectorAll('nav ul li a');
+                links.forEach(link => {
+                    link.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        window.location.href = link.getAttribute('href');
+                    });
+                });
+            })
+            .catch(error => console.error('Error loading navigation:', error));
+    }
 });
